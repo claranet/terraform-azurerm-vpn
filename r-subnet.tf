4a7f7637f1d9718189088f48b5022fb36ddc61a6
Jérémy MARMOL <jeremy.marmol@fr.clara.net>
2022-12-12 16:12:38 +0100

    AZ-908: Bump subnet module to 6.1.0

--- r-subnet.tf
+++ r-subnet.tf
@@ -1,6 +1,6 @@
 module "subnet_gateway" {
   source  = "claranet/subnet/azurerm"
-  version = "6.0.0"
+  version = "6.1.0"
 
   for_each = toset(var.subnet_gateway_cidr != null ? ["subnet_gw"] : [])
 
